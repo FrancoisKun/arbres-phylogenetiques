a1d9ad4ae0c61e5541fa5a9b6c7d2d061f664676
Mica <micamurphy42@gmail.com>
2017-11-29 15:22:38 +0100

    Premier jet de recherche et classifie.

--- arbresphylo.c
+++ arbresphylo.c
@@ -3,10 +3,36 @@
 
 
 int hauteur (noeud *racine) {
-    /* à compléter */
-    return 0;
+  /* à compléter */
+  return 0;
 }
 
 void recherche (noeud *racine, char *espece) {
-    /* à compléter */
+  if (racine.valeur == espece) {
+    printf("_");
+    return 0;
+  }
+  if (racine.valeur == NULL) {
+    printf("_");
+    return 1;
+  }
+  else {
+    printf("%s", racine.gauche.valeur); // TODO
+    if (recherche(racine.gauche, espece))
+      return 0; // TODO
+    printf("%s", racine.droit.valeur); // TODO
+    if (recherche(racine.droit, espece))
+      return 0; // TODO
+  }
+  return 1;
+}
+
+void classifie (noeud *racine, char *espece, int i) {
+  if (racine.gauche.valeur == NULL && racine.droit.valeur == NULL)
+    return 0;
+  else {
+    //ajouter(tab[i], racine); // TODO
+    classifie(racine.gauche, espece, i+1); // TODO
+    classifie(racine.droit, espece, i+1); // TODO
+  }
 }
